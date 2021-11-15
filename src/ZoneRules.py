810cad88096deb300d6f9c80038385ee669fbdab
MUSA KENAN MARASLI <e2037075@ceng.metu.edu.tr>
2021-11-15 22:48:45 +0300

    Add some rule choices to ZoneRules
    Ref #11

--- src/ZoneRules.py
+++ src/ZoneRules.py
@@ -1,4 +1,14 @@
 class ZoneRules(object):
     """Represents the rules of a zone."""
 
-    
+    # Binary rules and their default values are laid below.
+
+    # Is it OK to have people in this zone?
+    humansAreAllowed = True
+    # Is it OK for this zone to have noone in it?
+    HumanAbsenceIsAllowed = True
+    # Shall everyone wear a mask?
+    maskIsMandatory = False
+    # Are masks forbidden here?
+    maskIsForbidden = False
+    # @TODO: Add more rules.
