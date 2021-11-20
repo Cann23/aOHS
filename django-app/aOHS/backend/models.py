ab4a4dade8e52b736260cf22fbdcbf01e711337f
emre <e2405595@ceng.metu.edu.tr>
2021-11-20 19:55:12 +0300

    #18 create tables

--- django-app/aOHS/backend/models.py
+++ django-app/aOHS/backend/models.py
@@ -1,3 +1,62 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Camera(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id) + " " + str(self.name)
+
+
+class Worker(models.Model):
+    phone_regex = RegexValidator(regex=r'^\+905[0-9]{9}$',
+                                 message="Phone number should be in '+905555555555' format")
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    title = models.CharField(max_length=100, null=False)
+    phone = models.CharField(validators=[phone_regex], max_length=17, unique=True, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id) + " " + str(self.name)
+
+
+class Model(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    path = models.CharField(max_length=100, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id) + " " + str(self.name)
+
+
+class Violation(models.Model):
+    id = models.AutoField(primary_key=True)
+    cameraId = models.ForeignKey(Camera, on_delete=models.DO_NOTHING, null=False)
+    workerId = models.ForeignKey(Worker, on_delete=models.DO_NOTHING, null=True)
+    modelId = models.ForeignKey(Model, on_delete=models.SET_NULL, null=True)
+    comment = models.CharField(max_length=250, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Configuration(models.Model):
+    id = models.AutoField(primary_key=True)
+    cameraId = models.ForeignKey(Camera, on_delete=models.CASCADE, null=False)
+    modelId = models.ForeignKey(Model, on_delete=models.CASCADE, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
