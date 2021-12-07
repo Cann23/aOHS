f86b79e134ecc468c40fb8b0b3c99c69ee2e1344
MUSA KENAN MARASLI <e2037075@ceng.metu.edu.tr>
2021-12-07 21:02:12 +0300

    Add helmet-vest-worker module into main framework
    Ref #25

--- src/aohs.py
+++ src/aohs.py
@@ -1,3 +1,14 @@
+import sys
+import os
+
+# Add lib folder to sys path.
+p = os.path.dirname(os.path.abspath(__file__))
+p = os.path.join(p, "../lib/Helmet-Vest-Worker")
+p = os.path.abspath(p)
+sys.path.append(p)
+
+import helmetvestworker
+
 class aOHS(object):
     """Main program class."""
 
