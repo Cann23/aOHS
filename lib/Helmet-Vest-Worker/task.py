402b2c8b5c04ef1095c85159ecd2bfa9ebe7dc3a
emre <e2405595@ceng.metu.edu.tr>
2021-12-13 22:07:52 +0300

    #34 refactor

--- lib/Helmet-Vest-Worker/task.py
+++ lib/Helmet-Vest-Worker/task.py
@@ -2,6 +2,7 @@ import sqlite3
 import subprocess
 import threading
 from datetime import time
+import model_caller
 
 connection = sqlite3.connect('../../django-app/aOHS/db.sqlite3')
 cur_cursor = connection.cursor()
@@ -13,10 +14,11 @@ sql_get_config = "select c.id, m.id, m.path " \
                  "where c.active = 1 "
 
 
-def executeModel(camera_id, model_id, model_path):
-    command_str = f"python ./model_caller.py {camera_id} {model_id} {model_path} " + time().strftime("%m/%d%Y %H:%M:%S")
-    output = subprocess.check_output([command_str], shell=True)
-    print(output.decode("utf-8"))
+#
+# def executeModel(camera_id, model_id, model_path):
+#     command_str = f"python ./model_caller.py {camera_id} {model_id} {model_path} " + time().strftime("%m/%d%Y %H:%M:%S")
+#     output = subprocess.check_output([command_str], shell=True)
+#     print(output.decode("utf-8"))
 
 
 def TaskJob():
@@ -27,7 +29,7 @@ def TaskJob():
         thread_list = []
 
         for conf in result_set:
-            curr_thread = threading.Thread(target=executeModel, args=(conf[0], conf[1], conf[2],))
+            curr_thread = threading.Thread(target=model_caller.model_caller, args=(conf[0], conf[1], conf[2],))
             thread_list.append(curr_thread)
 
             curr_thread.start()
