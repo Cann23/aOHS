afef51a50b365ca7dc51992298a038dc060a3cd9
MUSA KENAN MARASLI <e2037075@ceng.metu.edu.tr>
2021-12-14 23:11:21 +0300

    Update Recorder to use camera proxy
    Ref #37

--- src/Recorder.py
+++ src/Recorder.py
@@ -1,21 +1,20 @@
 import threading
-import time
-import numpy
-import cv2
+
+import Camera
 
 class Recorder(object):
     """Records the capture as a video format file."""
 
     # Initializer for a recorder object.
-    # device: Device to record from.
+    # camera: Camera to record from.
     # fps: Frames per second.
     # max_duration: How long will this instance keep a recording window.
-    def __init__(self, device, fps, max_duration):
-        this.device = device
-        this.max_duration = max_duration
-        this.is_recording = False
-        this.frames = []
-        this._record_job_ = None
+    def __init__(self, camera : Camera.Camera, fps : int, max_duration : float):
+        self.camera = camera
+        self.max_duration = max_duration
+        self.is_recording = False
+        self.frames = []
+        self._record_job_ = None
 
     # Save the current data as a video file.
     # path: Path to save the file.
@@ -40,18 +39,9 @@ class Recorder(object):
     # Performs the recording
     def Record_Job(self):
         while is_recording:
-            # Begin capture.
-            cap = cv.VideoCapture(device)
-            if cap.isOpened():
-                ret, frame = cap.read()
-                # Cannot receive frame from camera?
-                if not ret:
-                    break
-                # Add frame to the list.
-                frames.append(frame)
-                # If the frame is filled, drop the first frame.
-                if frames.length() > fps * max_duration:
-                    frames.pop(0)
-        # Recording has stopped, release the capture.
-        if cap:
-            cap.release()
+            frame = self.camera.lastFrame
+            # Add frame to the list.
+            self.frames.append(frame)
+            # If the frame is filled, drop until it fits the buffer.
+            while frames.length() > fps * max_duration:
+                frames.pop(0)
