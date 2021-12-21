283a26b4cd33401667b236dd891e5406ad670a0f
ensarcoban <eyupenssr@gmail.com>
2021-12-21 15:18:43 +0300

    An extended user with two roles is added for now. It will give us flexibility to customize users in the feature. However, we won't need it this week.

--- django-app/aOHS/backend/models.py
+++ django-app/aOHS/backend/models.py
@@ -2,6 +2,10 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 class Camera(models.Model):
     id = models.AutoField(primary_key=True)
     url = models.URLField(max_length=100)
@@ -62,3 +66,22 @@ class Configuration(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ADMIN = 'AD'
+    SPECIALIST = 'SP'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (ADMIN, 'Admin'),
+        (SPECIALIST, 'Specialist'),
+    ]
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
