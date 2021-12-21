6dadcb9855bd8aab3e625c4bb537ff75cc89ade1
emre <e2405595@ceng.metu.edu.tr>
2021-12-21 17:40:30 +0300

    #39 create worker views

--- django-app/aOHS/dashboard/urls.py
+++ django-app/aOHS/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from dashboard.views import Example, Example2
+from dashboard.views import Example, Example2, WorkerView
 
 urlpatterns = [
     path('dashboard/', Example.as_view()),
     path('dashboard2/', Example2.as_view()),
+    path('dashboard/workers/', WorkerView.as_view()),
 ]
--- django-app/aOHS/dashboard/views.py
+++ django-app/aOHS/dashboard/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 from django.views import View
+
+from backend.models import Worker
 
 
 class Obj:
@@ -22,3 +23,25 @@ class Example2(View):
         data = [Obj(), Obj(), Obj()]
         return render(request, 'dashboard/list.html', {'headers': headers, 'data': data})
 
+
+class WorkerView(View):
+    def get(self, request):
+        headers = ['id', 'name', 'title', 'phone']
+        workers = Worker.objects.all()
+        return render(request, 'dashboard/worker_list.html', {'headers': headers, 'data': workers})
+
+    def post(self, request):
+        worker = Worker(name=request.POST['name'], title=request.POST['title'], phone=request.POST['phone'])
+        worker.save()
+        return redirect('dashboard/worker_list.html')
+
+    def put(self, request):
+        name = request.PUT['name']
+        title = request.PUT['title']
+        phone = request.PUT['phone']
+        Worker.objects.get(id=request.PUT['id']).update(name=name, title=title, phone=phone)
+        return redirect('dashboard/worker_list.html')
+
+    def delete(self, request):
+        Worker.objects.get(id=request.DELETE['id']).delete()
+        return redirect('dashboard/worker_list.html')
