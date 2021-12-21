faa3a5643635ae260d7fd3fe03ccb837fa318869
ensarcoban <eyupenssr@gmail.com>
2021-12-21 13:57:28 +0300

    dashboard.html and base.html that holds global styling is added.

--- django-app/aOHS/dashboard/urls.py
+++ django-app/aOHS/dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from backend.views import Example
+from dashboard.views import Example
 
 urlpatterns = [
     path('dashboard/', Example.as_view()),
