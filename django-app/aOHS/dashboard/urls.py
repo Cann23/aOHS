3b4d33bb910ed03a1faceb61337a0e2e9186a24e
ensarcoban <eyupenssr@gmail.com>
2021-12-21 14:57:46 +0300

    a half synamic list.html is written. Anyone can copy and change this file and use it by extending dashboard.html.

--- django-app/aOHS/dashboard/urls.py
+++ django-app/aOHS/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from dashboard.views import Example
+from dashboard.views import Example, Example2
 
 urlpatterns = [
     path('dashboard/', Example.as_view()),
+    path('dashboard2/', Example2.as_view()),
 ]
--- django-app/aOHS/dashboard/views.py
+++ django-app/aOHS/dashboard/views.py
@@ -4,7 +4,21 @@ from django.shortcuts import render
 from django.views import View
 
 
+class Obj:
+    def __init__(self):
+        self.x = 'x'
+        self.y = 'y'
+        self.z = 'z'
+
+
 class Example(View):
     def get(self, request):
         return render(request, 'dashboard/dashboard.html')
+
+
+class Example2(View):
+    def get(self, request):
+        headers = ['a', 'b', 'c']
+        data = [Obj(), Obj(), Obj()]
+        return render(request, 'dashboard/list.html', {'headers': headers, 'data': data})
 
