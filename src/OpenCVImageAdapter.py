556cfd459bf58045f4b3cdc37370c1921186059d
MUSA KENAN MARASLI <e2037075@ceng.metu.edu.tr>
2021-12-28 21:58:11 +0300

    Add image adapter for ml models from opencv
    Ref #45

---
+++ src/OpenCVImageAdapter.py
@@ -0,0 +1,12 @@
+import base.ImageAdapter as ia
+import base.Camera as cam
+
+class OpenCVImageAdapter(ia.ImageAdapter):
+    """Image adapter for ml that takes frames from openCV camera"""
+
+    def __init__(self):
+        pass
+
+    def GetImage(camera: cam.Camera):
+        frame = camera.lastFrame
+        raise NotImplementedError
