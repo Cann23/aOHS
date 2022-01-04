cfadd8c0347325c704d9ee7bae4fb88938fce8c8
MUSA KENAN MARASLI <e2037075@ceng.metu.edu.tr>
2022-01-04 22:24:32 +0300

    Implement save function in Recorder
    Ref #51

--- src/Recorder.py
+++ src/Recorder.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import cv2 as cv
 
 import base.Camera as Camera
 
@@ -13,6 +14,7 @@ class Recorder(object):
     def __init__(self, camera : Camera.Camera, fps : int, max_duration : float):
         self.camera = camera
         self.max_duration = max_duration
+        self.fps = fps
         self.is_recording = False
         self.frames = []
         self.__record_job__ = None
@@ -21,14 +23,15 @@ class Recorder(object):
     # path: Path to save the file.
     # delay: How long to wait before saving the file.
     def Save(self, path, delay):
-        pass
+        __save_job__ = threading.Thread(target=self.__SaveJob__, args=(self, path, delay))
+        __save_job__.start()
 
     # Starts recording.
     def StartRecording(self):
         if is_recording:
             return
         is_recording = True
-        self.__record_job__ = threading.Thread(target=self.__RecordJob__)
+        self.__record_job__ = threading.Thread(target=self.__RecordJob__, args=(self,), daemon=True)
         self.__record_job__.start()
 
     # Stops recording.
@@ -47,4 +50,14 @@ class Recorder(object):
             while frames.length() > fps * max_duration:
                 frames.pop(0)
 
-    
+    # Performs the video saving.
+    def __SaveJob__(self, path, delay):
+        # Delay saving if requested.
+        if delay > 0:
+            time.sleep(delay)
+        # Begin writing.
+        writer = cv.VideoWriter(path, cv.VideoWriter_fourcc(*'MJPG'), self.fps, self.camera.size)
+        for frame in self.frames:
+            writer.write(frame)
+        # Finish writing.
+        writer.release()
