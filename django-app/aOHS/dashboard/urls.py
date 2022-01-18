fc9291de798ac6717778db40d5eb1c9887cb7a12
emre <e2405595@ceng.metu.edu.tr>
2022-01-18 16:49:08 +0300

    #63 add remove button to the configuration list

--- django-app/aOHS/dashboard/urls.py
+++ django-app/aOHS/dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from dashboard.views.camera_views import CameraView, CameraCreateView, CameraEditView
-from dashboard.views.configuration_views import ConfigurationView, ConfigurationCreateView, ConfigurationEditView
+from dashboard.views.configuration_views import ConfigurationView, ConfigurationCreateView, ConfigurationEditView, \
+    ConfigurationDeleteView
 from dashboard.views.model_views import ModelView, ModelEditView
 from dashboard.views.statistic_views import StatisticView
 from dashboard.views.violation_views import ViolationView, ViolationCreateView, ViolationEditView, ViolationDeleteView
@@ -22,6 +23,7 @@ urlpatterns = [
     path('dashboard/configurations/', ConfigurationView.as_view()),
     path('dashboard/configurations/add/', ConfigurationCreateView.as_view()),
     path('dashboard/configurations/edit/<int:configuration_id>/', ConfigurationEditView.as_view()),
+    path('dashboard/configurations/delete/<int:configuration_id>/', ConfigurationDeleteView.as_view()),
     path('dashboard/cameras/', CameraView.as_view()),
     path('dashboard/cameras/add/', CameraCreateView.as_view()),
     path('dashboard/cameras/edit/<int:camera_id>/', CameraEditView.as_view()),
--- django-app/aOHS/dashboard/views/configuration_views.py
+++ django-app/aOHS/dashboard/views/configuration_views.py
@@ -6,6 +6,7 @@ from django.views import View
 from backend.models import Configuration, Camera, Model
 import datetime
 
+
 class ConfigurationView(View):
     def get(self, request):
         headers = ["id", "cameraId", "modelId", "created", "modified"]
@@ -35,14 +36,15 @@ class ConfigurationCreateView(View):
     def post(self, request):
         cameras = Camera.objects.all()
         models = Model.objects.all()
-        camera =cameras.get(id= request.POST['camera'])
-        model =models.get(id= request.POST['model'])
-        configuration=Configuration(cameraId= camera, modelId= model)
+        camera = cameras.get(id=request.POST['camera'])
+        model = models.get(id=request.POST['model'])
+        configuration = Configuration(cameraId=camera, modelId=model)
         configuration.save()
         return redirect('/dashboard/configurations/')
 
+
 class ConfigurationEditView(View):
-    def get(self, request,configuration_id):
+    def get(self, request, configuration_id):
         configuration = Configuration.objects.get(id=configuration_id)
         return render(request, 'dashboard/configuration-edit.html', {'configuration': configuration})
 
@@ -50,4 +52,10 @@ class ConfigurationEditView(View):
         cameraId = request.POST['cameraId']
         modelId = request.POST['modelId']
         Configuration.objects.filter(id=configuration_id).update(cameraId=cameraId, modelId=modelId)
+        return redirect('/dashboard/configurations/')
+
+
+class ConfigurationDeleteView(View):
+    def get(self, request, configuration_id):
+        Configuration.objects.get(id=configuration_id).delete()
         return redirect('/dashboard/configurations/')
