b31035f97cebab737b55135c416791e41695164a
Kenan Marasli <musakenanmarasli@gmail.com>
2022-01-18 21:34:30 +0300

    Add description to DetectionResult class
    Ref #66

--- src/base/DetectionResult.py
+++ src/base/DetectionResult.py
@@ -1,4 +1,5 @@
 class DetectionResult(object):
+    """Represents the result of a detection. It is a base class for more concrete result classes (e.g HelmetDetectionResult)."""
 
     def __init__(self, camera):
         self.camera = camera
