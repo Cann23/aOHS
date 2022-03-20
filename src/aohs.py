12d7162634b0b58761fff0b300c2223169d3e466
Kenan Marasli <e2037075@ceng.metu.edu.tr>
2022-03-20 14:36:17 +0300

    Add aiortc handlers
    Ref #75

--- src/aohs.py
+++ src/aohs.py
@@ -1,5 +1,11 @@
 import sys
 import os
+import argparse
+import json
+import logging
+import ssl
+import uuid
+
 from OpenCVCamera import *
 from Scheduler import *
 
@@ -9,13 +15,118 @@ p = os.path.join(p, "../lib/Helmet-Vest-Worker")
 p = os.path.abspath(p)
 sys.path.append(p)
 
+ROOT = os.path.dirname(__file__)
+
+logger = logging.getLogger("pc")
+pcs = set()
+
 class aOHS(object):
     """Main program class."""
 
     def __init__(self):
         self.scheduler = Scheduler()
-        self.cameras = None
+        self.cameras = {}
 
     # Creates and / or deletes cameras.
     def UpdateCameras(self):
         raise NotImplementedError
+
+    def GetCamera(self, cameraid):
+       self.cameras[cameraid]
+
+program = aOHS()
+
+class OpenCVVideoTrack(MediaStreamTrack):
+    """Track that takes an OpenCV camera for streaming"""
+
+    def __init__(self, OpenCVCamera):
+        super().__init__()
+        self.camera = OpenCVCamera
+
+    async def recv(self):
+        return self.camera.lastFrame
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    log_info("Created for %s", request.remote)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("getcamera")
+    def on_getcamera(cameraid):
+        log_info("Get camera id: %s received", cameraid)
+
+        track = OpenCVVideoTrack(program.GetCamera(cameraid))
+        pc.addTrack(track)
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Camera get id: %s ended", cameraid)
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+ 
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="aOHS Video Stream Server"
+    )
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_post("/offer", offer)
+    web.run_app(
+        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+    )
+
