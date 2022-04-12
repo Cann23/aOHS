a12fed2358f374d5afd169d5c81d08f698590916
ensarcoban <eyupenssr@gmail.com>
2022-04-12 21:25:16 +0300

    #97 Profile picture added to user model. Minor errors are debugged.

--- django-app/aOHS/backend/admin.py
+++ django-app/aOHS/backend/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Worker, Camera, Configuration, Model, Violation
+from .models import Worker, Camera, Configuration, Model, Violation, Profile
 
 admin.site.register(Worker)
 admin.site.register(Camera)
 admin.site.register(Configuration)
 admin.site.register(Model)
 admin.site.register(Violation)
+admin.site.register(Profile)
--- django-app/aOHS/backend/models.py
+++ django-app/aOHS/backend/models.py
@@ -76,6 +76,7 @@ class Configuration(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
     ADMIN = 'AD'
     SPECIALIST = 'SP'
     ROLE = [
