af60ef90570703c8d1b7af9ee555d017b4275f81
Kenan Marasli <e2037075@ceng.metu.edu.tr>
2022-05-09 01:55:33 +0300

    #115 Fix formating

--- django-app/aOHS/static/stream/client.js
+++ django-app/aOHS/static/stream/client.js
@@ -1,64 +1,64 @@
 var pc = null;
 
 function negotiate() {
-    pc.addTransceiver('video', {direction: 'recvonly'});
-    return pc.createOffer().then(function(offer) {
-        return pc.setLocalDescription(offer);
-    }).then(function() {
-        // wait for ICE gathering to complete
-        return new Promise(function(resolve) {
-            if (pc.iceGatheringState === 'complete') {
-                resolve();
-            } else {
-                function checkState() {
-                    if (pc.iceGatheringState === 'complete') {
-                        pc.removeEventListener('icegatheringstatechange', checkState);
-                        resolve();
-                    }
-                }
-                pc.addEventListener('icegatheringstatechange', checkState);
-            }
-        });
-    }).then(function() {
-        var offer = pc.localDescription;
-        var urlParams = new URLSearchParams(window.location.search);
-        return fetch('offer/', {
-            body: JSON.stringify({
-                sdp: offer.sdp,
-                type: offer.type,
-                url: escape(urlParams.get('url'))
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'POST'
-        });
-    }).then(function(response) {
-        return response.json();
-    }).then(function(answer) {
-        return pc.setRemoteDescription(answer);
-    }).catch(function(e) {
-        alert(e);
-    });
+	pc.addTransceiver('video', {direction: 'recvonly'});
+	return pc.createOffer().then(function(offer) {
+		return pc.setLocalDescription(offer);
+	}).then(function() {
+		// wait for ICE gathering to complete
+		return new Promise(function(resolve) {
+			if (pc.iceGatheringState === 'complete') {
+				resolve();
+			} else {
+				function checkState() {
+					if (pc.iceGatheringState === 'complete') {
+						pc.removeEventListener('icegatheringstatechange', checkState);
+						resolve();
+					}
+				}
+				pc.addEventListener('icegatheringstatechange', checkState);
+			}
+		});
+	}).then(function() {
+		var offer = pc.localDescription;
+		var urlParams = new URLSearchParams(window.location.search);
+		return fetch('offer/', {
+			body: JSON.stringify({
+				sdp: offer.sdp,
+				type: offer.type,
+				url: escape(urlParams.get('url'))
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: 'POST'
+		});
+	}).then(function(response) {
+		return response.json();
+	}).then(function(answer) {
+		return pc.setRemoteDescription(answer);
+	}).catch(function(e) {
+		alert(e);
+	});
 }
 
 function start() {
-    var config = {
-        sdpSemantics: 'unified-plan'
-    };
-    pc = new RTCPeerConnection(config);
-    pc.addEventListener('track', function(evt) {
-        if (evt.track.kind == 'video') {
-            document.getElementById('video').srcObject = evt.streams[0];
-        }
-    });    
-    negotiate();
+	var config = {
+		sdpSemantics: 'unified-plan'
+	};
+	pc = new RTCPeerConnection(config);
+	pc.addEventListener('track', function(evt) {
+		if (evt.track.kind == 'video') {
+			document.getElementById('video').srcObject = evt.streams[0];
+		}
+	});	
+	negotiate();
 }
 
 function stop() {
-    setTimeout(function() {
-        pc.close();
-    }, 500);
+	setTimeout(function() {
+		pc.close();
+	}, 500);
 }
 
 start();
